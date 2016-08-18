80070ce60a71b439afe4b38bd059dd5d9df8cfb7
Niole Nelson <niolenelson@gmail.com>
2016-08-17 22:16:22 -0700

    can add and multiply properly

--- calculator.scala
+++ calculator.scala
@@ -1,4 +1,7 @@
 object Calculator {
+  type Unparsed = List[String]
+  type Parsed = (Unparsed, Expression)
+
   abstract class Expression {
     def calculate: Int = this match {
       case Number(n) => n
@@ -9,18 +12,85 @@ object Calculator {
     }
   }
 
+  def tokenize(input: String): Unparsed = input.split(" ").toList
+
   case class Number(n: Int) extends Expression
   case class Add(l: Expression, r: Expression) extends Expression
   case class Subtract(l: Expression, r: Expression) extends Expression
   case class Multiply(l: Expression, r: Expression) extends Expression
   case class Divide(l: Expression, r: Expression) extends Expression
 
+  object Number {
+    def parse(input: Unparsed): Parsed =
+      (input.tail, Number(input.head.toInt))
+  }
+
+  object Multiply {
+    def parse(input: Unparsed): Parsed = input match {
+      case Nil => (Nil, Number(1))
+      case left :: rest =>
+      if (!rest.isEmpty) {
+        val operator = rest.head
+        val tail = rest.tail
+        if (operator == "*" || operator ==  "/") {
+          val (unparsed, parsed) = Multiply.parse(tail)
+
+          (unparsed, Multiply(Number(left.toInt), parsed))
+        }
+        else (rest, Number(left.toInt))
+      } else Number.parse(input)
+    }
+  }
+
+  object Add {
+    def parse(input: Unparsed, agg: Expression): Parsed = input match {
+      case Nil => (Nil, Number(0))
+      case left :: rest =>
+        if (!rest.isEmpty) {
+          val operator = rest.head
+          val tail = rest.tail
+
+          if (operator == "*" || operator == "/") {
+            val (unparsed, parsed) = Multiply.parse(input)
+            val sum = Add(parsed, agg)
+
+            if (!unparsed.isEmpty) Add.parse("0" :: unparsed, sum)
+            else (unparsed, sum)
+          }
+          else {
+            val (unparsed, parsed) = Add.parse(tail, agg)
+            (unparsed, Add(agg, Add(Number(left.toInt), parsed)))
+          }
+        } else Number.parse(input)
+    }
+  }
 }
 
 object Test {
   import Calculator._
   def run: Unit = {
-    val exp = "1 + 2 * 3"
+    val shouldBe11 = "1 + 2 * 3 + 4"
+    val _11s = tokenize(shouldBe11)
+    val _11 = Add.parse(_11s, Number(0))._2.calculate
+    println(_11 == 11)
+
+    val shouldBe7 = "1 + 2 * 3"
+    val _7s = tokenize(shouldBe7)
+    val _7 = Add.parse(_7s, Number(0))._2.calculate
+    println(_7 == 7)
+
+    val shouldBe6 = "0 * 1 + 2 * 3"
+    val _6s = tokenize(shouldBe6)
+    val _6 = Add.parse(_6s, Number(0))._2.calculate
+    println(_6 == 6)
+
+    val shouldBe2half = "1 / 2 + 2 - 0"
+    val _2halfs = tokenize(shouldBe2half)
+    val _2half = Add.parse(_2halfs, Number(0))._2.calculate
+    println(_2half)
+    println(_2half == 2.5)
+
+
     println(Add(Number(2), Number(2)).calculate == 4)
     println(Add(Multiply(Number(2), Number(2)), Number(2)).calculate == 6)
     println(Subtract(Multiply(Number(2), Number(2)), Number(2)).calculate == 2)
